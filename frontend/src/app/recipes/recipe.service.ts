3836bb989d9332e1caa1b395723a1399475e3803
Arthur Barbosa <arthurbruno03@gmail.com>
2022-06-19 14:02:35 -0300

    Passing ingredients from recipes to the shopping list (via service)

--- frontend/src/app/recipes/recipe.service.ts
+++ frontend/src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.mode';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -8,11 +10,18 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A teste Recipe', 'This is a simply test', 'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg')
+    new Recipe('A teste Recipe', 'This is a simply test', 'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg', [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ])
   ]
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice()
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
